a8129d30f14ed82f2ea22cc749aa87455cbe791b
inimaul <222410102052@mail.unej.ac.id>
2024-03-08 17:41:43 +0700

    Tugas Percobaan API

---
+++ Models/PersonContext.cs
@@ -0,0 +1,26 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class PersonContext
+    {
+        public List<Person> ListPerson()
+        {
+            List<Person> list1 = new List<Person>();
+            Object[,] arrayPerson = new Object[,]{
+                {1, "Ulul", "Gresik", "lullll@gmail.com" },
+                {2, "Yusril", "Warlok Jember", "yusss@gmail.com" },
+                {3, "Ave", "Lumajang", "veee@gmail.com" }
+            };
+            for (int i = 0; i < arrayPerson.GetLength(0); i++)
+            {
+                list1.Add(new Person()
+                {
+                    id_person = int.Parse(arrayPerson[i, 0].ToString()),
+                    nama = arrayPerson[i, 1].ToString(),
+                    alamat = arrayPerson[i, 2].ToString(),
+                    email = arrayPerson[i, 3].ToString()
+                });
+            }
+            return list1;
+        }
+    }
+}
